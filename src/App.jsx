72b0d855763239d25afb2a29e3146bf9414dfb30
Harshad <harshad.hm2002@gmail.com>
2025-07-12 03:13:18 +0530

    logo cgage and name change

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   
 
   return (
-    <BrowserRouter>
+    <BrowserRouter  basename="/Appointment-Calender" >
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="Calender" element={<Calender/>} />
